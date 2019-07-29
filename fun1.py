c8d4bafc7fad4101826ac0199c1445f03d0ec304
asaad21-meet <asaad21@meet.mit.edu>
2019-07-29 03:27:57 -0400

    Stopping here

--- fun1.py
+++ fun1.py
@@ -1,9 +1,18 @@
-def add_numbers():
+
+def add_number():
     total=0
     for number in range (1,10+1):
         print(number)
         total = total + number
-    return(total)
-
-answer=add_numbers()
+    return(total)   
+"""
+answer=add_number()
 print(answer)
+"""
+def add_numbers(start,end):
+    test=0
+    for i in range(start,end+1):
+        test+=i
+    return(test)
+x=add_numbers(1,10)                
+print(x)        
---
+++ fun2.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.goto(0,0)
+
+
+def up():
+    print('you passed the up key.')
+
+turtle.onkey(up,'Up')
+turtle.goto(0,0)
+turtle.listen()
